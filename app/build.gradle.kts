727ceb568fb49474c62d2940940dcd4b6efde1db
qijin <513848731@qq.com>
2025-10-11 17:29:16 +0800

    my application demo5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/myapplication/VideoPlay.kt
@@ -0,0 +1,67 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+
+class VideoPlay : AppCompatActivity() {
+    private lateinit var videoView: VideoView
+    private lateinit var inputUrl: EditText
+    private lateinit var playButton: Button
+    private lateinit var selectButton: Button
+    private val PICK_VIDEO_REQUEST_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.video_play)
+
+        videoView = findViewById(R.id.videoView)
+        inputUrl = findViewById(R.id.urlInput)
+        playButton = findViewById(R.id.playButton)
+        selectButton = findViewById(R.id.selectButton)
+
+        playButton.setOnClickListener {
+            val url = inputUrl.text.toString()
+            if (url.isNotEmpty()) {
+                playVideo(url.toUri())
+            }
+        }
+
+        selectButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "video/*"
+                addCategory(Intent.CATEGORY_OPENABLE)
+            }
+            startActivityForResult(intent, PICK_VIDEO_REQUEST_CODE)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_VIDEO_REQUEST_CODE && resultCode == RESULT_OK) {
+            data?.data?.let { uri ->
+                playVideo(uri)
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun playVideo(url: Uri) {
+        videoView.setVideoURI(url)
+        videoView.start()
+    }
+}
